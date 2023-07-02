e099cf8a6e61013f93058b29d20674442ec282d6
adityamahrjan <122766850+adityamahrjan@users.noreply.github.com>
2023-07-01 22:12:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // change nav style on scroll
-window.addEventListener('scroll', ()
-=> {
+window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
-})
+});
 
 
 //contact buttons (circular text buttons)
@@ -11,5 +10,62 @@ const textButtons = document.querySelectorAll('.contact__btn');
 textButtons.forEach(textButton => {
     let text = textButton.querySelector('p');
 
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<spanstyle="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('');
+});
+
+
+
+
+//SWIPER JS (gallery section)
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 4
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 6
+        }
+    }
+});
+
+
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+
+openNavBtn.addEventListener('click', openNav);
+
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
+
+closeNavBtn.addEventListener('click', closeNav);
+
+if(document.body.clientWidth < 1024) {
+  nav.querySelectorAll('li a').forEach(navLink => {
+    navLink.addEventListener('click', closeNav);
+  })
+}
+
+
+
+
+
+
